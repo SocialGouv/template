59630d1c2748eb2e7e732fa1c9bf7c7f6b181264
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-02-12 16:37:55 +0100

    feat: add ts support

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -3,7 +3,7 @@
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
 
-describe("Basic", () => {
+describe("Basic spec", () => {
   it("should render the main page", () => {
     cy.visit("http://localhost:3000/");
   });
