474fd206c269e16c5e60d10d21937f9a2b5c3991
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2022-03-11 18:35:35 +0100

    fix: stories

--- src/components/stats/tile.stories.tsx
+++ src/components/stats/tile.stories.tsx
@@ -14,8 +14,7 @@ const Template: ComponentStory<typeof StatsTile> = args => (
 export const Default = Template.bind({});
 Default.args = {
   title: "Nombre de visites",
-  stats: "1.000.000",
-  description: "C'est le nombre d'utilisateur unique ayant visité le site",
+  stats: "1.000.000"
 };
 
 export const WithDescription = Template.bind({});
