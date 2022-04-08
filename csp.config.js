21500646093e040c81abf321b0a9b7465c772805
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-04-08 15:08:28 +0200

    fix: authentificatio

--- csp.config.js
+++ csp.config.js
@@ -1,10 +1,14 @@
 const ContentSecurityPolicy = `
-  default-src 'self' *.fabrique.social.gouv.fr;
+  default-src 'self' *.fabrique.social.gouv.fr  ${
+    process.env.NODE_ENV !== "production" && "http://localhost:8080"
+  };
   img-src 'self' data: *.fabrique.social.gouv.fr https://dummyimage.com/;
   script-src 'self' *.fabrique.social.gouv.fr ${
     process.env.NODE_ENV !== "production" && "'unsafe-eval'"
   };
-  frame-src 'self' *.fabrique.social.gouv.fr;
+  frame-src 'self' *.fabrique.social.gouv.fr  ${
+    process.env.NODE_ENV !== "production" && "http://localhost:8080"
+  };
   style-src 'self' 'unsafe-inline';
   font-src 'self' data: blob:;
 `;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@gouvfr/dsfr/dist/dsfr/dsfr.min.css";
-import type { AppProps } from "next/app";
+import type { AppProps, AppContext } from "next/app";
 import SEO from "../../next-seo.config";
 import { Layout } from "@components";
 import {
@@ -12,8 +12,23 @@ import {
 import { useEffect } from "react";
 import { init } from "@socialgouv/matomo-next";
 import { DefaultSeo } from "next-seo";
+import cookie from "cookie";
+import * as React from "react";
+import type { IncomingMessage } from "http";
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { SSRKeycloakProvider, SSRCookies } from "@react-keycloak/ssr";
+
+const keycloakCfg = {
+  url: "http://localhost:8080",
+  realm: "realme-app",
+  clientId: "react-client",
+};
+
+interface InitialProps {
+  cookies: unknown;
+}
+
+function MyApp({ Component, pageProps, cookies }: AppProps & InitialProps) {
   useEffect(() => {
     init({
       url: process.env.NEXT_PUBLIC_MATOMO_URL ?? "",
@@ -22,19 +37,39 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <Layout
-      headerProps={{ ...headerProps }}
-      footerProps={{
-        bodySection: footerBodySection,
-        bottomSection: footerBottomSection,
-        partnerSection: footerPartnerSection,
-        topSection: footerTopSection,
-      }}
+    <SSRKeycloakProvider
+      keycloakConfig={keycloakCfg}
+      persistor={SSRCookies(cookies)}
     >
-      <DefaultSeo {...(SEO as any)} />
-      <Component {...pageProps} />
-    </Layout>
+      <Layout
+        headerProps={{ ...headerProps }}
+        footerProps={{
+          bodySection: footerBodySection,
+          bottomSection: footerBottomSection,
+          partnerSection: footerPartnerSection,
+          topSection: footerTopSection,
+        }}
+      >
+        <DefaultSeo {...(SEO as any)} />
+        {/* <SessionProvider session={pageProps.session}> */}
+
+        <Component {...pageProps} />
+
+        {/* </SessionProvider> */}
+      </Layout>
+    </SSRKeycloakProvider>
   );
 }
+
+function parseCookies(req: IncomingMessage) {
+  return cookie.parse(req.headers.cookie || "");
+}
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  // Extract cookies from AppContext
+  return {
+    cookies: context.ctx.req ? parseCookies(context.ctx.req) : {},
+  };
+};
 
 export default MyApp;
