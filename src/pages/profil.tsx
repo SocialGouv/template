b0f976104cce23035c308872b8d5ab4a844aeef9
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-04-15 16:45:46 +0200

    fix: use of next-auth

--- src/pages/profil.tsx
+++ src/pages/profil.tsx
@@ -1,38 +1,23 @@
+import React from "react";
 import { withAuth } from "@lib";
-import { useKeycloak } from "@react-keycloak/ssr";
-import { ParsedToken } from "@types";
-import type { KeycloakInstance } from "keycloak-js";
+import { useSession } from "next-auth/react";
 
 const IndexPage = () => {
-  const { keycloak } = useKeycloak<KeycloakInstance>();
-  const parsedToken: ParsedToken | undefined = keycloak?.tokenParsed;
-
+  const { data: session } = useSession();
   return (
     <div className="fr-container fr-my-6w">
       <h1>Mes informations</h1>
       <div className="fr-px-3w">
-        {keycloak?.authenticated ? (
-          <ul>
-            <li>
-              <span className="fr-text--bold">Email : </span>
-              <span>{parsedToken?.email ?? ""}</span>
-            </li>
-            <li>
-              <span className="fr-text--bold">Username : </span>
-              <span>{parsedToken?.preferred_username ?? ""}</span>
-            </li>
-            <li>
-              <span className="fr-text--bold">Prénom : </span>
-              <span>{parsedToken?.given_name ?? ""}</span>
-            </li>
-            <li>
-              <span className="fr-text--bold">Nom de famille : </span>
-              <span>{parsedToken?.family_name ?? ""}</span>
-            </li>
-          </ul>
-        ) : (
-          <p>Identifiez vous pour accédez à ces informations</p>
-        )}
+        <ul>
+          <li>
+            <span className="fr-text--bold">Email : </span>
+            <span>{session?.user?.email ?? ""}</span>
+          </li>
+          <li>
+            <span className="fr-text--bold">Name : </span>
+            <span>{session?.user?.name}</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
