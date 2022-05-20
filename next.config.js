2b87cf4022f0f069409ed1ea07ada1de7b597d2a
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-05-20 10:33:04 +0200

    fix: variable

--- next.config.js
+++ next.config.js
@@ -13,25 +13,6 @@ const moduleExports = {
   },
   env: {
     NEXT_PUBLIC_APP_VERSION: version,
-    NEXT_PUBLIC_APP_VERSION_COMMIT: process.env.GITHUB_SHA,
-    NEXT_PUBLIC_IS_PRODUCTION_DEPLOYMENT: process.env.PRODUCTION,
-    NEXT_TELEMETRY_DISABLED: process.env.NEXT_TELEMETRY_DISABLED ?? 1,
-    NEXT_PUBLIC_SITE_URL: process.env.SITE_URL ?? "",
-    NEXT_PUBLIC_SENTRY_DSN: process.env.SENTRY_DSN ?? "",
-    NEXT_PUBLIC_SENTRY_ENV: process.env.SENTRY_ENV ?? "",
-    NEXT_PUBLIC_MATOMO_URL: process.env.MATOMO_URL ?? "",
-    NEXT_PUBLIC_MATOMO_SITE_ID: process.env.MATOMO_SITE_ID ?? "",
-    NEXT_PUBLIC_APP_REPOSITORY_URL:
-      process.env.APP_REPOSITORY_URL ??
-      "https://github.com/SocialGouv/template",
-    KEYCLOAK_URL:
-      process.env.KEYCLOAK_URL ?? "http://localhost:8080/auth/realms/app-realm",
-    KEYCLOAK_CLIENT_SECRET: process.env.KEYCLOAK_CLIENT_SECRET ?? "**********",
-    KEYCLOAK_CLIENT_ID: process.env.KEYCLOAK_CLIENT_ID ?? "app-client",
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL ?? "http://localhost:3000",
-    NEXTAUTH_SECRET:
-      process.env.NEXTAUTH_SECRET ??
-      "RnDOzMROgGQqGE6zJ9Vx+vPoQn/x4Y1zmaz/Xj+xg0I=",
   },
 };
 
