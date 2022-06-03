f12b401b276cd7f4fac688d7fbc12e7337aeb333
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-06-03 21:33:25 +0200

    fix: version

--- keycloak/scripts/keycloak.sh
+++ keycloak/scripts/keycloak.sh
@@ -2,4 +2,6 @@
 
 export NEXTAUTH_URL=http://localhost:3000 
 export KEYCLOAK_CLIENT_SECRET=********** 
+export FRANCE_CONNECT_CLIENT_ID=123
+export FRANCE_CONNECT_CLIENT_SECRET=123 
 cat .kube-workflow/files/realm-export.json | gomplate > keycloak/config/realm-export-local.json
