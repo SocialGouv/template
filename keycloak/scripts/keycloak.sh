33c519bcf0f0aac0f197c6ac9044ae255ce0cb86
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-06-03 10:41:26 +0200

    fix: script

--- keycloak/scripts/keycloak.sh
+++ keycloak/scripts/keycloak.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-NEXTAUTH_URL=http://localhost:3000 KEYCLOAK_CLIENT_SECRET=********** cat .kube-workflow/files/realm-export.json | gomplate > keycloak/config/realm-export-local.json
+export NEXTAUTH_URL=http://localhost:3000 
+export KEYCLOAK_CLIENT_SECRET=********** 
+cat .kube-workflow/files/realm-export.json | gomplate > keycloak/config/realm-export-local.json
