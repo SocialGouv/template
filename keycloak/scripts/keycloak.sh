79bbe1302bd229c465a2ebf1aae025ee036a754c
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-06-07 21:25:06 +0200

    fix: add SMTP_SSL

--- keycloak/scripts/keycloak.sh
+++ keycloak/scripts/keycloak.sh
@@ -3,5 +3,8 @@
 export NEXTAUTH_URL=http://localhost:3000 
 export KEYCLOAK_CLIENT_SECRET=********** 
 export FRANCE_CONNECT_CLIENT_ID=123
-export FRANCE_CONNECT_CLIENT_SECRET=123 
+export FRANCE_CONNECT_CLIENT_SECRET=123
+export SMTP_HOST=smtp.tipimail.com
+export SMTP_USER=smtp-user
+export SMTP_PASSWORD=smtp-password
 cat .kube-workflow/files/realm-export.json | gomplate > keycloak/config/realm-export-local.json
