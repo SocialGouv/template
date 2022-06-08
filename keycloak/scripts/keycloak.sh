8d482b4453eaa41d7a18efe5d8719aed80d831e4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-06-08 09:58:04 +0200

    Update keycloak.sh

--- keycloak/scripts/keycloak.sh
+++ keycloak/scripts/keycloak.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script creates keycloak/config/realm-export-local.json for local docker-compose developement
+
 export NEXTAUTH_URL=http://localhost:3000 
 export KEYCLOAK_CLIENT_SECRET=********** 
 export FRANCE_CONNECT_CLIENT_ID=123
