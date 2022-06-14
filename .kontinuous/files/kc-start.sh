6ab4e3942ec8b17e7308444fc5495b0fc1e872f4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-06-14 18:05:22 +0200

    fix

--- .kontinuous/files/kc-start.sh
+++ .kontinuous/files/kc-start.sh
@@ -2,5 +2,12 @@
 
 # import realm and start server for keycloak@17 - fixed in 18
 
+ls
+ls /opt/keycloak/data/import
+/opt/keycloak/bin/kc.sh --version
+
 /opt/keycloak/bin/kc.sh import --dir /opt/keycloak/data/import
-/opt/keycloak/bin/kc.sh start --auto-build --db=postgres --features admin2 --http-enabled=true --http-port=8080 --hostname-strict=false --hostname-strict-https=false
+
+/opt/keycloak/bin/kc.sh --version
+
+/opt/keycloak/bin/kc.sh start doc --db=postgres --features admin2 --http-enabled=true --http-port=8080 --hostname-strict=false --hostname-strict-https=false
