439f13defde8c7703ae1abd607e07e31f8a70745
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-09-29 19:19:27 +0200

    feat: add sentry example triggers (#103)

--- src/pages/pageA.tsx
+++ src/pages/pageA.tsx
@@ -1,12 +1,22 @@
-import { Alert } from "@dataesr/react-dsfr";
+import { Alert, Button } from "@dataesr/react-dsfr";
 import type { NextPage } from "next";
 import React from "react";
 
 const PageA: NextPage = () => {
+  const onClick1 = () => {
+    throw new Error("Hello, sentry");
+  };
+  const onClick2 = () => {
+    fetch("/api/error");
+  };
   return (
     <div className="fr-container fr-my-6w">
       <h1>Lien A</h1>
       <Alert title="Cette page doit être mise à jour. " type="info" />
+      <br />
+      <Button onClick={onClick1}>test sentry client-side error</Button>
+      &nbsp;
+      <Button onClick={onClick2}>test sentry server-side error</Button>
     </div>
   );
 };
