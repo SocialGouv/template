f92679a52da660a4c593c6efba61e30535d63bee
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-10-21 19:47:39 +0200

    restore

--- next.config.js
+++ next.config.js
@@ -7,18 +7,6 @@ const ContentSecurityPolicy = require("./csp.config");
 /** @type {import('next').NextConfig} */
 const moduleExports = {
   reactStrictMode: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   sentry: {
     disableClientWebpackPlugin: true,
     disableServerWebpackPlugin: true,
