f7dee3ff89206201f6bd14d4015490847ff93f28
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-02-02 20:00:31 +0100

    e2e

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,10 +1,7 @@
 describe("Home page", () => {
   it("should render the main page", () => {
     cy.visit("http://localhost:3000/");
-    cy.title().should(
-      "equal",
-      "Template | Fabrique numérique des ministères sociaux"
-    );
+    cy.title().should("equal", "'Template SocialGouv");
     cy.get("h1").should(
       "contain",
       "Template de la fabrique des ministères sociaux"
