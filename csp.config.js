66958890d2d964456fc09308342703e2cc9bd252
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-02-14 11:20:10 +0100

    fix: csp

--- csp.config.js
+++ csp.config.js
@@ -1,7 +1,8 @@
 const ContentSecurityPolicy = `
   default-src 'self' *.fabrique.social.gouv.fr;
   img-src 'self' data: *.fabrique.social.gouv.fr https://dummyimage.com/;
-  script-src 'self' *.fabrique.social.gouv.fr ${
+  connect-src 'self' *.lafabriquenumerique.cloud-ed.fr;
+  script-src 'self' *.fabrique.social.gouv.fr *.lafabriquenumerique.cloud-ed.fr ${
     process.env.NODE_ENV !== "production" && "'unsafe-eval' 'unsafe-inline'"
   };
   frame-src 'self' *.fabrique.social.gouv.fr;
