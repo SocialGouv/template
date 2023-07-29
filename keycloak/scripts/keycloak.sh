ad1fcf0621755e1a41ea7309da5e4b13773d45d5
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-07-29 02:20:19 +0200

    fix

--- keycloak/scripts/keycloak.sh
+++ keycloak/scripts/keycloak.sh
@@ -6,8 +6,10 @@ export NEXTAUTH_URL=http://localhost:3000
 export KEYCLOAK_CLIENT_SECRET=********** 
 export FRANCE_CONNECT_CLIENT_ID=123
 export FRANCE_CONNECT_CLIENT_SECRET=123
-export SMTP_HOST=smtp.tipimail.com
-export SMTP_USER=smtp-user
-export SMTP_PASSWORD=smtp-password
+export SMTP_HOST=maildev
+export SMTP_PORT=1025
+export SMTP_USER=some
+export SMTP_PASSWORD=some
 export SMTP_SSL=false
-cat .kontinuous/files/realm-export.json | gomplate > keycloak/config/realm-export-local.json
+
+gomplate < .kontinuous/files/realm-export.json > keycloak/config/realm-export-local.json
