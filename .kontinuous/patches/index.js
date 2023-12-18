a2542f571131bcfa439c53971b423eccff5c4843
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-12-18 11:23:36 +0100

    chore: add example patch

---
+++ .kontinuous/patches/index.js
@@ -0,0 +1,14 @@
+/*
+Patch manifests
+*/
+module.exports = (manifests) => {
+  for (const manifest of manifests) {
+    const { kind } = manifest;
+    if (kind === "Ingress") {
+      manifest.metadata.labels = {
+        ...manifest.metadata.labels,
+        hello: "world",
+      };
+    }
+  }
+};
