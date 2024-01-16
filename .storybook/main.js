f52ee2f629257e274d4b59bb4899c0353b11ec9d
devthejo <devthejo@protonmail.com>
2024-01-16 14:25:06 +0100

    fix: storybook up to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,13 +2,19 @@ const path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-dark-mode",
   ],
-  framework: "@storybook/react",
+
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
       test: /\.mjs$/,
@@ -17,5 +23,10 @@ module.exports = {
     });
     return config;
   },
+
   staticDirs: ["./static"],
+
+  docs: {
+    autodocs: true,
+  },
 };
