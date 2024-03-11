588cbcffb2703de3446dc3762c9e4545d9af889f
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-03-11 08:58:01 +0100

    cypress

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,13 +1,7 @@
 describe("Home page", () => {
   it("should render the main page", () => {
     cy.visit("http://localhost:3000/");
-    cy.title().should(
-      "equal",
-      "Template | Fabrique numérique des ministères sociaux"
-    );
-    cy.get("h1").should(
-      "contain",
-      "Template de la fabrique des ministères sociaux"
-    );
+    cy.title().should("equal", "Template | beta.gouv.fr");
+    cy.get("h1").should("equal", "Template");
   });
 });
