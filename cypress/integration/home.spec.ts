c58703edbd29987f30a3b0a567f08146a81ebd74
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-03-11 09:26:08 +0100

    cypress

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -2,6 +2,6 @@ describe("Home page", () => {
   it("should render the main page", () => {
     cy.visit("http://localhost:3000/");
     cy.title().should("equal", "Template | beta.gouv.fr");
-    cy.get("h1").should("equal", "Template");
+    cy.get("h1").should("contain", "Template");
   });
 });
